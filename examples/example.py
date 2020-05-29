e38275a331c5319f6e49e5776ba3087a25378da2
m1ch3al <renato.sirola@gmail.com>
2020-05-29 23:20:53 +0200

    include setup package

---
+++ examples/example.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+"""
+Released under the MIT License
+Copyright 2015 MrTijn/Tijndagamer
+"""
+
+# Import the MPU6050 class from the MPU6050.py file
+from adafruit_mpu6050 import MPU6050
+from time import sleep
+import os
+
+# Create a new instance of the MPU6050 class
+sensor = MPU6050(0x68)
+
+while True:
+    accel_data = sensor.get_accel_data()
+    gyro_data = sensor.get_gyro_data()
+    temp = sensor.get_temp()
+
+    print("Accelerometer data")
+    print("x: " + str(accel_data['x']))
+    print("y: " + str(accel_data['y']))
+    print("z: " + str(accel_data['z']))
+
+    print("\nGyroscope data")
+    print("x: " + str(gyro_data['x']))
+    print("y: " + str(gyro_data['y']))
+    print("z: " + str(gyro_data['z']))
+
+    print("\nTemp: " + str(temp) + " C")
+    sleep(0.1)
+    os.system("clear")
